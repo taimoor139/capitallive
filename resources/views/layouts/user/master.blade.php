0556c52db7dd08dcc378d096d031570889661d24
Mostakim <mimrzs2013@gmail.com>
2022-03-24 17:21:33 +0600

    template mastering

---
+++ resources/views/layouts/user/master.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html class="loading dark-layout" lang="en" data-layout="dark-layout" data-textdirection="ltr">
+<!-- BEGIN: Head-->
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
+    <meta name="description"
+        content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+    <meta name="keywords"
+        content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="PIXINVENT">
+    <title>Dashboard ecommerce - Vuexy - Bootstrap HTML admin template</title>
+
+    @include('layouts.user.partials.style')
+
+</head>
+<!-- END: Head-->
+
+<!-- BEGIN: Body-->
+
+<body class="vertical-layout vertical-menu-modern  navbar-floating footer-static  " data-open="click"
+    data-menu="vertical-menu-modern" data-col="">
+
+    <!-- BEGIN: Header-->
+    @include('layouts.user.partials.header')
+
+
+    <!-- BEGIN: Main Menu-->
+    @include('layouts.user.partials.sidebar')
+    <!-- END: Main Menu-->
+
+    <!-- BEGIN: Content-->
+    @yield('content')
+    <!-- END: Content-->
+
+    <div class="sidenav-overlay"></div>
+    <div class="drag-target"></div>
+
+    <!-- BEGIN: Footer-->
+    @include('layouts.user.partials.footer')
+    <!-- END: Footer-->
+    @include('layouts.user.partials.script')
+
+</body>
+<!-- END: Body-->
+
+</html>
